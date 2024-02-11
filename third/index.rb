d2d51468f2c614335469c475f932407e42fe6dad
Taisei <taisei96.web@gmail.com>
2024-02-12 08:33:57 +0900

    復習

--- third/index.rb
+++ third/index.rb
@@ -57,7 +57,40 @@ a = a ** 2 # 200 ** 2 = 40000
 
 ## 引数のデフォルト値
 
-def foo(a, b=100)
-  a + b
-end
-p foo # ArgumentError
+# def foo(a, b=100)
+#   a + b
+# end
+# p foo # ArgumentError
+
+## キーワード引数
+
+# def foo(a:, b: 100)
+#   a + b 
+# end
+
+# foo(a: 2, b: 3)
+# foo(a: 1)
+
+## キーワード引数に任意の引数を使う
+
+# def bar(a:, b: 100, **z)
+#   p z
+#   a + b
+# end
+# bar(a: 2, c:100, d:200)
+
+### 論理値（難しい）
+
+## 論理演算子
+
+### 文字列リテラルと式展開
+
+## 文字列リテラル
+
+a = "abcd"
+p a
+p a.class
+b = "ab"'cd'
+p b
+
+## 式展開
