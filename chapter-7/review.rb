e9491cf23d3288e0a88eb56ae3bacd6be108a0be
Taisei <taisei96.web@gmail.com>
2024-02-24 11:18:51 +0900

    3章

--- chapter-7/review.rb
+++ chapter-7/review.rb
@@ -96,22 +96,49 @@ puts Piyo.new.message # Piyoクラスのインスタンス化
 # => Hello
 
 # 継承/オーバーライド
-class Job1 # スーパークラス
-  def myjob_job1
-    puts "私の仕事は教師です。"
-  end
-end 
 
-class Job2 < Job1 # サブクラス
-  def myjob 
-    puts "私の仕事はエンジニアです。"
-  end 
-end 
+## 別名のメソッド定義
+# class Job1 # スーパークラス
+#   def myjob_job1
+#     puts "私の仕事は教師です。"
+#   end
+# end 
 
-job = Job2.new # Job2のインスタンス作成
-p job.myjob_job1
+# class Job2 < Job1 # サブクラス
+#   def myjob 
+#     puts "私の仕事はエンジニアです。"
+#   end 
+# end 
+
+# job = Job2.new # Job2のインスタンス作成
+# job.myjob_job1 # => 私の仕事は教師です。
+
+## 同名のメソッド定義　
+# class Job3 
+#   def myjob
+#     puts "私の仕事は教師です。"
+#   end 
+# end 
+
+# class Job4 < Job3 
+#    def myjob
+#     puts "私の仕事はエンジニアです。"
+#   end 
+# end 
+
+# job = Job4.new # Job4のインスタンス作成
+# job.myjob # => 私の仕事はエンジニアです。
 
 =begin 
+
+rubyでは、サブクラス（子クラス）がスーパークラス（親クラス）を継承している状態で異なる名前のメソッドを定義していれば、サブクラスのインスタンスに対してスーパークラスのメソッドを実行することができる。
+
+同じメソッド定義
+・継承していてもスーパークラスのメソッドは実行されず、サブクラスのメソッドが実行される
+
+オーバーライド
+・サブクラスの中でスーパークラスで定義されているメソッドと同じ名前のメソッドを定義できる。
+「super」というメソッドを使うとスーパークラスで定義したメソッドが使える
 
 =end 
 
